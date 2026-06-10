a27b0e2c6ab40469d1fbf74878e89c4be21b3b18
genesissssssss <bautistamarcgenesis@gmail.com>
2026-06-10 23:57:44 +0800

    models continue

--- courses/models.py
+++ courses/models.py
@@ -62,4 +62,29 @@ created_at = models.DateTimeField(auto_now_add=True)
 updated_at = models.DateTimeField(auto_now=True)
 published_at = models.DateTimeField(blank=True, null=True)
 
+class Meta:
+    ordering = ['-created_at']
+    indexes = [
+        models.Index(fields=['slug']),
+        models.Index(fields=['level', 'is_published']),
+    ]
+def __str__(self):
+    return self.title
 
+def save (self, *args, **kwargs):
+    if self.is_published and not self.published_at:
+        self.published_at = timezone.now()
+    super().save(*args, **kwargs)
+
+    @property 
+    def total_modules(self):
+        return self.modules.count()
+    @property
+    def total_duration_minutes(self):
+        """Calculate total course duration from video lessons"""
+        total = 0
+        for module in self.modules.all():
+            for lesson in module.lessons.all():
+                if lesson.duration:
+                    total += lesson.duration
+        return total
