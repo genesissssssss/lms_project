d01f4a21becc39d257dd1994bac6fdb2f25855ac
genesissssssss <bautistamarcgenesis@gmail.com>
2026-06-28 01:16:28 +0800

    admin complete

--- courses/admin.py
+++ courses/admin.py
@@ -37,3 +37,53 @@ class CourseAdmin(admin.ModelAdmin):
 
     inlines = [ModuleInline]
     actions = ['publish_courses', 'unpublish_courses']
+
+    def total_modules_display(self, obj):
+       return obj.modules.count()
+    total_modules_display.short_description = 'Modules'
+    
+    def unpublish_courses(self, request, queryset):
+        updated = queryset.update(is_published=False)
+        self.message_user(request, f'{updated} courses unpublished')
+    unpublish_courses.short_description = 'unpublished selected courses'
+
+@admin.register(Module)
+class moduleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'courses_link', 'order', 'lesson_count']
+    list_filter = ['course']
+    search_fields = ['title', 'description']
+    inlines = [LessonInline]
+
+    def course_link(self, obj):
+        url = reverse('admin:courses_course_change', args=[obj.course.id])
+        return format_html('<a href= "{}">{}</a>', url, obj.course.title)
+    course_link.short_description = 'Course'
+
+    def lesson_count(self, obj):
+        return obj.lesson.count()
+    lesson_count.short_description = 'Lesson'
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'module_link', 'content_type', 'order', 'is_free_preview', 'duration']
+    list_filter = ['content_type', 'is_free_preview', 'module__course']
+    search_fields = ['title', 'description']
+
+    def module_link(self, obj):
+        url = reverse('admin:courses_module_change', args=[obj.module.id])
+        return format_html('<a href="{}">{}</a>', url, obj,module.title)
+    module_link.short_description = 'Module'
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ['student', 'course', 'status', 'progress_percentage', 'enrolled_at']
+    list_filter = ['status', 'course']
+    search_fields = ['student__username', 'course__title']
+    readonly_fields = ['enrolled_at', 'last_accessed']
+
+
+@admin.register(LessonProgress)
+class LessonProgressAdmin(admin.ModelAdmin):
+    list_display =['enrollment', 'lesson', 'is_completed', 'quiz_score', 'time_spent']
+    list_filter = ['is_completed', 'lesson__content_type']
+    search_fields = ['enrollment__student__username', 'lesson__title']
